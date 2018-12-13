df86cb8a1f6f1bf3e0c75027c385a2a7ffd5ba7f
SPY001720 <45824217+SPY001720@users.noreply.github.com>
2018-12-13 18:44:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,62 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const cfg require('./index.json'); // a garder en version desktop
+const cfg = require('./index.json'); // a garder en version desktop
 const token = process.env.token // a garder en version heroku
-const prefix = ("?");
+var prefix = ("!");
+var bot = new Discord.Client();
 
-bot.on('ready', function () {
-    console.log("Je suis prêt à être utilisé.")
-    bot.user.setActivity('rien').catch(console.error)
+const token = process.env.token
+
+bot.on('ready', () => {
+    bot.user.setPresence({ game: { name: '[!Mystik] <🔱 𝓢𝓟𝓨 🔱>', type: 0}})
+    console.log("Bot pret");
 });
 
 bot.on('guildMemberAdd', member => {
     member.createDM().then(channel => {
-        return channel.send('Bienvenue sur le serveur de Alexpgm' + member.displayName)
-        console.log(`${member.displayName} à rejoind le serveur.`)
+        return channel.send('Bienvenue sur MystikRP !' + member.displayName)
     }).catch(console.error)
 });
+    
+    bot.on('message', message => {
+        if (message.content === "!mention"){
+            message.reply("")
+        }
+    
+        if (message.content === prefix+ "Mystik"){
+            var help_embed = new Discord.RichEmbed()
+                .setColor('#B9121B')
+                .addField("Liste des commandes :"," Voici les commandes du Bot !")
+                .addField("!mention", "Cette commande permet de savoir son nom discord en se mentionnant.")
+                .addField("!infos", "Cette commande permet de vous informer sur le serveur.")
+                .addField("!staff", "Cette commande est utile si vous avez besoins de parler a un staff d'un certain grade , elle permet d'afficher la liste des staff de MystikRP ainsi que leur grade.")
+            message.channel.sendEmbed(help_embed);
+        }
+    
+        if (message.content === prefix+ "infos"){
+            var help_embed = new Discord.RichEmbed()
+                .setColor('#AEEE00')
+                .addField("Informations :","MystikRP est un serveur Rôleplay qui est composé d'un map personnalisé crée par Pelisma (Mappeur de MystikRP), MystikRP contient parfois ses propres addons !")
+                .addField("Nom du serveur :", "[FR] MYSTIK ROLEPLAY | Map & Addons exclus | LoloxCommunity.com")
+                .addField("IP :", "164.132.116.56:27260")
+                .addField("Gamemode :", "DarkRP")
+                .addField("Site Web :", "https://loloxcommunity.com/")
+            message.channel.sendEmbed(help_embed);
+        }
+    
+        if (message.content === prefix+ "staff"){
+            var help_embed = new Discord.RichEmbed()
+                .setColor('#AEEE00')
+                .addField("Staff du serveur :"," MystikRP")
+                .addField("Fondateur", "Lolox")
+                .addField("Mappeur", "Pelisma")
+                .addField("Superadmins", "Shadow\n Kobralost\n Stylaxx")
+                .addField("Administrateurs", "Skyflix\n Alex Backo\n El Famoso Marvino\n 🔱 𝓢𝓟𝓨 🔱\n ChickenNuggets (Hoie)\n A Fromage de chevre\n Petit enfant arrogant")
+            message.channel.sendEmbed(help_embed);
+        }
+    
+    });
 
-const ban = require('./kick et ban/ban');
+bot.login(token)
 
 
-bot.on('message', function (message){
-    if (ban.match(message)){
-        return ban.action(message)
-    }
-});
-
-
-bot.on('message', msg => {
-    if (msg.content === "bonjour"){
-        msg.reply("Heureux de te revoir parmis nous.")
-    }
-    if (msg.content.match(/salut/i)) {
-            msg.reply('Je suis d\'accord avec toi.')
-    }
-    if (msg.content === prefix + "site"){
-        msg.channel.send("https://alexpgm.000webhostapp.com/")
-        console.log("Une personne a demandé pour aller sur ton site.")
-    }
-
-});
-
-bot.login(cfg.token); //a garder en version desktop
-bot.login(token); //a garder en version heroku
